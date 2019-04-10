de934867d85e79ace0d07a4d16f430e9cb9994f3
YDM <ydmno16@naver.com>
2019-04-10 14:39:50 +0900

    create dto class

---
+++ gitTest/src/kr/co/dto/DeptDTO.java
@@ -0,0 +1,29 @@
+package kr.co.dto;
+
+public class DeptDTO {
+	private int deptno;
+	private String dname;
+	private String loc;
+	
+	public DeptDTO() {
+		super();
+	}
+	public int getDeptno() {
+		return deptno;
+	}
+	public void setDeptno(int deptno) {
+		this.deptno = deptno;
+	}
+	public String getDname() {
+		return dname;
+	}
+	public void setDname(String dname) {
+		this.dname = dname;
+	}
+	public String getLoc() {
+		return loc;
+	}
+	public void setLoc(String loc) {
+		this.loc = loc;
+	}
+}
